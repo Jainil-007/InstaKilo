fd65cf0729731f43954f9dd9ef8870e59ef0eabe
jainil ptl <abcdfrd@gail.com>
2021-05-22 10:49:46 +1200

    Initial layout with logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import logoo from '../src/Logo.PNG';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">     
+
+    
+     <div className="app_header">
+      <img
+      className="app_headerImage"
+      height="35"
+       src={logoo}
+       alt=""
+       />
+
+     </div>
+ <h1>Hey Jainil</h1>
+     {/*posts*/}
+     {}
     </div>
   );
 }
